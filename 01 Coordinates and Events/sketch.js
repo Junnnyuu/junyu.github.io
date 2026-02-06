cb6f56feb46bfa9fc00048804f053c3e525d9230
Junnnyuu <juntang391@gmail.com>
2026-02-06 12:18:09 -0600

    test

--- 01 Coordinates and Events/sketch.js
+++ 01 Coordinates and Events/sketch.js
@@ -11,14 +11,71 @@
 // draw() -> runs over and over (after setup)
 
 // screen is updated at the bottom of draw
+
+
+//    ------  Global variable section -------
+
+// simple data types
+
+let circleX = 50;
+
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(200,200);
 }
 
-function draw() {
-  background(220);
 
-  //      x   y  d
-  circle(0,0,500);
+
+
+function draw() {
+
+  //repeats over and over (automatically) 60fps
   
+  background(600);
+
+  drawTwoCircle();
+  drawCircle();
+  drawTable();
+  
+}
+
+
+function drawTwoCircle()
+{
+  // //      R   G  B
+  // fill(200,10,20);
+
+  // stroke(100,1000,10);
+  // //      x   y  d
+  // circle(circleX,50,50);
+
+
+  // fill(2000,10,20);
+
+  // stroke(100,1000,10);
+  // //      x   y  d
+  // circle(width/2,height/2,150);
+
+}
+
+function drawCircle()
+{
+  stroke(10,100,100);
+
+  circle(30,80,10);
+
+  circle(60,80,10);
+
+  circle(60,100,10);
+
+  circle(30,100,10);
+}
+
+
+
+function drawTable()
+{
+  stroke(10,10,100);
+  rect(30,80,30,20);
+
 }
