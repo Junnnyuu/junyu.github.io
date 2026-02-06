0eb16aedbb92b0b080210b24a02490e143fb9297
Junnnyuu <juntang391@gmail.com>
2026-02-06 11:07:56 -0600

    aa

---
+++ 01 Coordinates and Events/sketch.js
@@ -0,0 +1,24 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// In python - run- to- completion
+
+// p5js - we write interactive 
+
+// setup() -> we write once at the start
+// draw() -> runs over and over (after setup)
+
+// screen is updated at the bottom of draw
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+
+  //      x   y  d
+  circle(0,0,500);
+  
+}
