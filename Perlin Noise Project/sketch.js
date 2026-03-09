3558b510c509e6ef896837b287808a6149aeaa0b
Junnnnm <juntang391@gmail.com>
2026-03-08 22:44:10 -0600

    mouse

--- Perlin Noise Project/sketch.js
+++ Perlin Noise Project/sketch.js
@@ -5,54 +5,42 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-
-let rectWidth = 1;
-let noiseSet = 0;
-let noiseValue = 0;
-
+let changeValue = 0;
+let t = 0;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   generateTerrain();
 }
 
-function generateTerrain(){
-  let highestHeight = -Infinity;
-  let highestX = 0;
-  let highestY = 0;
-  // use a loop to generate and draw
-  // several rectangles side to side
-  // to look like some 2D terrain
-  rectMode(CORNERS);
 
-  for(let x = 0; x < width; x += rectWidth){
-    //generate a random height. 
-    //change this from using random() to noise()
-    let rectHeight = map(noise(noiseValue), 0, 1, 50, 1000); 
-    
-    //calculate the other corner of our rectangle
-    let x2 = x + rectWidth;
-    let y2 = height - rectHeight;
-
-    rect(x, height, x2, y2);
-
-    if (rectHeight > highestHeight) 
-    {
-      highestHeight = rectHeight;
-      highestX = x + rectWidth / 2;
-      highestY = y2;
-    }
-
-    totalHeight += rectHeight; 
-    count++;
-
-    noiseValue += 0.01;
-  }
-  
-  rectMode(CORNER);
-}
 
 function draw() {
-  background(220);
+  //background(220);
+}
+
+
+
+function generateTerrain()
+{
+  for (let x = 0; x < width; x += changeValue)
+  {
+    let y = noise(x/100 + t) * height;
+    rect(x, y, 1, height - y);
+  }
+  t += 0.01;
+}
+
+
+function  keyPressed()
+{
   generateTerrain();
-  noiseValue += 0.05;
+
+  if(key == "left")
+  {
+    changeValue -= 0.5;
+  }
+  if(key == "right")
+  {
+    changeValue += 0.5;
+  }
 }
