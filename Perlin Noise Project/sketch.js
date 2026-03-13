2190f85ae8c47182e2de4548bc65d5d397118b7c
Junnnnm <juntang391@gmail.com>
2026-03-12 23:10:43 -0600

    update
    -- Flag, keyboard, fix bug with noise

--- Perlin Noise Project/sketch.js
+++ Perlin Noise Project/sketch.js
@@ -5,42 +5,54 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let changeValue = 0;
+let changeValue_2 = 0.01;
 let t = 0;
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  drawFlag(20,20);
   generateTerrain();
 }
 
 
 
 function draw() {
-  //background(220);
+  background(220);
+  generateTerrain();
+  drawFlag(20,20);
 }
 
 
 
 function generateTerrain()
 {
-  for (let x = 0; x < width; x ++)
+  let changeValue = changeValue_2;
+  for (let x = 0; x < width; x+=changeValue)
   {
-    let y = noise(x*0.01, t) * height;
+    let y = noise(x*0.01) * height;
     
-    rect(x, y, 1, height - y);
+    rect(x, y, changeValue, height - y);
   }
 }
 
 
 function  keyPressed()
 {
-  generateTerrain();
+  if(keyCode === LEFT_ARROW)
+  {
+    changeValue_2 -= 5;
+    changeValue_2 = max(changeValue_2, 1); 
+  }
+  if(keyCode === RIGHT_ARROW)
+  {
+    changeValue_2 += 5;
+    changeValue_2 = min(changeValue_2, width);
+  }
 
-  if(key === "left")
-  {
-    t -= 0.5;
-  }
-  if(key === "right")
-  {
-    t += 0.5;
-  }
+}
+
+
+function drawFlag(x,y)
+{
+  rect(x, y, x-20, y-10);
+  triangle(x, y, x, y-10, x+10, y-5);
 }
