4f67e19c5d0185b3d3d91957b0d91b9ce9507739
Junnnnm <juntang391@gmail.com>
2026-03-14 15:13:04 -0600

    update
    -- correcct the flag

--- Perlin Noise Project/sketch.js
+++ Perlin Noise Project/sketch.js
@@ -5,7 +5,7 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let changeValue_2 = 0.01;
+let changeValue_2 = 0.1;
 let t = 0;
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -18,20 +18,37 @@ function setup() {
 function draw() {
   background(220);
   generateTerrain();
-  drawFlag(20,20);
 }
 
 
 
 function generateTerrain()
 {
+  let highestHeight = -Infinity;
   let changeValue = changeValue_2;
+  let highestX = 0;
+  let highestY = 0;
+
   for (let x = 0; x < width; x+=changeValue)
   {
-    let y = noise(x*0.01) * height;
+    let y = noise(x*0.02) * height;
     
     rect(x, y, changeValue, height - y);
+
+    if (height - y > highestHeight)
+    {
+      highestHeight = height - y;
+      highestX = x + changeValue/2;
+      highestY = y;
+    }
   }
+
+  drawFlag(highestX, highestY - 5);
+
+
+
+
+
 }
 
 
@@ -39,12 +56,12 @@ function  keyPressed()
 {
   if(keyCode === LEFT_ARROW)
   {
-    changeValue_2 -= 5;
+    changeValue_2 -= 0.5;
     changeValue_2 = max(changeValue_2, 1); 
   }
   if(keyCode === RIGHT_ARROW)
   {
-    changeValue_2 += 5;
+    changeValue_2 += 0.5;
     changeValue_2 = min(changeValue_2, width);
   }
 
@@ -53,6 +70,6 @@ function  keyPressed()
 
 function drawFlag(x,y)
 {
-  rect(x, y, x-20, y-10);
-  triangle(x, y, x, y-10, x+10, y-5);
+  line(x, y, x, y+10);
+  triangle(x, y, x, y-6, x+5, y-3);
 }
