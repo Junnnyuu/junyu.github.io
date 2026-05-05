f5d657454c64caf050b33e35e44057681fabbc9d
Junnnyuu <juntang391@gmail.com>
2026-05-04 22:15:05 -0600

    update

--- Puzzle Game/sketch.js
+++ Puzzle Game/sketch.js
@@ -6,10 +6,98 @@
 // - describe what you did to take this project "above and beyond"
 
 
+let grid = [
+  [0,   0,   0,   255,  0,  255],
+  [255, 0, 255,   0,    255,  0],
+  [0,   0,   0,   0,    0,  255],
+  [255, 255, 255, 255,  255,  0],
+  [0,   255, 0,   0,    0,  255]
+];
+let rows = grid.length;
+let cols = grid[0].length;
+let tileSize = 60;
+let currentRow, currentCol;
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(cols*tileSize, rows*tileSize);
 }
 
 function draw() {
   background(220);
+  determineActive();
+  renderGrid();
+  textSize(20);
+  fill(255,0,0);
+  //text(getCurrentX()+","+getCurrentY(),mouseX, mouseY)
+}
+
+
+function mousePressed(){
+  // cross-shaped pattern flips on a mouseclick. Boundary conditions are checked within the flip function to ensure in-bounds access for array
+
+  if (keyIsDown(SHIFT)){   // if it is shift, Only the current flip is executed
+    flip (currentCol, currentRow);
+    return;
+  }
+    flip(currentCol, currentRow);
+    flip(currentCol-1, currentRow);
+    flip(currentCol+1, currentRow);
+    flip(currentCol, currentRow-1);
+    flip(currentCol, currentRow+1);
+}
+
+function determineActive(){
+  // An expression to run each frame to determine where the mouse currently is.
+  currentRow = int(mouseY / rectHeight);
+  currentCol = int(mouseX / rectWidth);
+}
+
+
+
+
+function flip(x,y){
+  if(grid[y][x] === 0) grid[y][x] = 255;
+  else grid[y][x] = 0;
+}
+
+function mousePressed(){
+  //only do a flip if mouse is on the Canvas
+  if(mouseX < width && mouseY < height){
+    
+    let x = getCurrentX();
+    let y = getCurrentY();
+
+    // ALWAYS:
+    flip(x, y);
+
+    // IF THEY EXIST:
+    // flip the cardinal (NSEW) neighbours
+    if(x-1 >= 0) flip(x-1, y); //LEFT
+    if(y-1 >= 0) flip(x, y-1); //UP
+  }
+  
+}
+
+function renderGrid(){
+  // intepret the data stored in 2D array (grid) and
+  // draw a matrix of squares to reflect it
+  for(let y = 0; y < rows; y++){ //y:0 1 2 3 4
+    for(let x = 0; x < cols; x++){ //x: 0 1 2 3 4 5
+      let fillColor = grid[y][x];
+      fill(fillColor);
+      square(x*tileSize, y*tileSize, tileSize);
+    }
+  }
+}
+
+function getCurrentX(){
+  //determine the current col position of mouse
+  let constrainedX = constrain(mouseX, 0, width-1);
+  return floor(constrainedX / tileSize);
+}
+
+function getCurrentY(){
+  //determine the current row position of mouse
+  let constrainedY = constrain(mouseY, 0, height-1);
+  return floor(constrainedY / tileSize);
 }
