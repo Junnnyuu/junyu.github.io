24cbdf59fc9c242ae290d3d7f6c9dc4a070f1fcf
Junnnyuu <juntang391@gmail.com>
2026-05-13 12:17:31 -0600

    github

---
+++ 24 Objects, inheritance/c.js
@@ -0,0 +1,26 @@
+class LineObject extends AnimatedObject
+{
+    constructor()
+    {
+        super(random(width), random(height));
+    }
+
+    move()
+    {
+        super.move();
+        this.x -= 5;
+
+        if(this.x < 0 ) this.x = width;
+    }
+
+    display()
+    {
+        if(mouseIsPressed)
+        {
+            strokeWeight(22);
+        }
+        else strokeWeight(2);
+
+        line(this.x, this.y, this.x + 15, this.y);
+    }
+}
