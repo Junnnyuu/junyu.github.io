d32c1e04a7c81fa14daf59d72bfe4c69dbaa5685
Junnnyuu <juntang391@gmail.com>
2026-05-15 12:10:49 -0600

    update
    add more picture

--- Final project/sketch.js
+++ Final project/sketch.js
@@ -5,17 +5,27 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let player;
+let player_1, player_2, player_3;
+
+let birdie;
+
 
 function preload()
 {
-  player = loadImage("assets/player_idle_1.png");
+  player_1 = loadImage("assets/player_idle_1.png");
+  player_2 = loadImage("assets/player_idle_2.png");
+  player_3 = loadImage("assets/player_idle_3.png");
+  birdie = loadImage("assets/badminton_birdie_1.png");
+  //player_4 = loadImage("assets/player_idle_4.png");
 }
 
 function setup() {
-  createCanvas(player.width,player.height);
+  createCanvas(200,200);
   pixelDensity(1);
-  image(player, 0, 0);
+  image(player_1, 10, 100);
+  image(player_2, 10, 100);
+  image(birdie, 10, 10,30,50);
+  //image(player_1, 10, 100);
   loadPixels();
 }
 
